b58cac9a66faa37ce4a3f0a4e2159942d36a074b
Kuanwen Lai <kwlai0927@gmail.com>
2020-06-18 11:37:29 +0800

    fix env name.

--- server.py
+++ server.py
@@ -22,8 +22,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
 
 @app.route("/callback", methods=['POST'])
