ae8583ef251aff9cefcf9a240ce5a927cc7f4210
Kuanwen Lai <kwlai0927@gmail.com>
2020-06-19 10:25:33 +0800

    fd

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = ""
+    message = event.message.text
     
     _guesser = Guesser.all_guessers.setdefault(event.source.userId, Guesser())
     if _guesser.state == 'from':
